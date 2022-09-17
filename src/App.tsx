02818a04d8dbaa96d6fff94543cafb7b373f220c
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-09-17 16:49:26 +0800

    fix: style

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Banner } from "~/components/banner";
 
 function App() {
   return (
-    <div>
+    <div className="h-screen flex flex-row justify-center items-center">
       <Banner />
     </div>
   );
